92f520fab0a838990be8efe630bc4d1f12471e47
Joalbert Gonzalez <joalbertgonzalez@gmail.com>
2017-02-07 13:20:16 -0400

    py1:Implementing method that checks prime numbers

--- primes.py
+++ primes.py
@@ -1,16 +1,23 @@
 """
-Your module documentation here
+Encapsulation method for verifying prime numbers
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Definition of the method is_prime
     """
-
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Any number less than 2 is not a prime number
+        We go through the numbers, starting with the number
+        two up to the one ingressed.
+        We check if its residue is equal to zero.
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int < 2:
+            return False
+        for sequential in range(2, num_int):
+            if num_int % sequential == 0:
+                return False
+        return True
